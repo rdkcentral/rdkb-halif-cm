e43b5791ff13c2231cb146397733d02f6b5ec665
hgd296 <162419216+hgd296@users.noreply.github.com>
2024-03-07 11:44:05 +0530

    Update build_ut.sh

--- build_ut.sh
+++ build_ut.sh
@@ -20,26 +20,39 @@
 # *
 
 # Clone the Unit Testing Suit for this repo, it follows a standard convention
-# HAL Name is replaced with haltest, allowing this script to be part of the template for all
+# halif Name is by default to halif-test, allowing this script to be part of the template for all hals
+# This is a general configuration for ease, you can hard code it if that's the requirement
 
 # This will look up the last tag in the git repo, depending on the project this may require modification
-TEST_REPO=$(git remote -vv | head -n1 | awk -F ' ' '{print $2}' | sed 's/hal/haltest/g')
+# We need to check for the old format, and also support either old or new depending on the repo.
+# Transition was from hal -> hal-test, new format is from halif -> halif-test
+TEST_REPO=$(git remote -vv | head -n1 | awk -F ' ' '{print $2}' | sed 's/halif/halif-test/g' | sed 's/hal-/haltest-/g' )
+DIR="."
 
-# Set default UT_PROJECT_VERSION to master
+# Set default UT_PROJECT_VERSION to main
 if [ -z "${UT_PROJECT_VERSION}" ]; then
-    UT_PROJECT_VERSION=master
+    UT_PROJECT_VERSION=main
 fi
-
-UT_DIR="./ut"
 
 # Simple help
 if [ "${1}" == "-h" ]; then
     echo "Script to build the unit testing suite"
     echo " build_ut.sh <clean> - clean the testing"
     echo " build_ut.sh TARGET=xxx - build the xxx version of the tests, linux/arm etc."
+    echo " build_ut.sh --dir xxx - Build Directory (must be first)"
     echo " build_ut.sh <noswitch> - build the linux version of tests using skeleton & stubs"
     exit 0
 fi
+
+PARAMS=$@
+if [ "${1}" == "--dir" ]; then
+# Remove the --dir & the command from the switches
+    shift
+    DIR=${1}
+    shift
+fi
+
+UT_DIR="${DIR}/ut"
 
 # Check if the common document configuration is present, if not clone it
 if [ -d ${UT_DIR} ]; then
@@ -47,10 +60,12 @@ if [ -d ${UT_DIR} ]; then
     ./build.sh $@
     popd > /dev/null
 else
-    echo "Cloning unit Test Suite for this module"
+    echo "Cloning unit Test Suite for this module [${DIR}]"
+    mkdir -p ${DIR}
+    pushd ${DIR} > /dev/null
     git clone ${TEST_REPO} ut
-    pushd ${UT_DIR} > /dev/null
+    cd ${UT_DIR}
     git checkout ${UT_PROJECT_VERSION}
     popd > /dev/null
-    ./${0} $@
+    ./${0} ${PARAMS}
 fi
